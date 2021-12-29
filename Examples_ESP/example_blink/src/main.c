fb5fe2fca3626402008f34e0d6e004dfeeb262f8
Stefan de Kraker <stefandekraker1999@gmail.com>
2021-12-29 01:29:51 +0100

    New code standard

--- Examples_ESP/example_blink/src/main.c
+++ Examples_ESP/example_blink/src/main.c
@@ -1,29 +1,38 @@
-// Needed to control GPIO
+/*
+ * Author:      Stefan de Kraker
+ * Version:     1.0
+ *
+ * Simple example to show the use of a GPIO Output
+*/
+
+/* Needed to control GPIO */
 #include "driver/gpio.h"
-// Include FreeRTOS for delay 
+/* Include FreeRTOS for delay */
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-// Built-in LED on the ESP32 DEVKIT C1 is on pin 2
-// pinout https://www.mischianti.org/wp-content/uploads/2020/11/ESP32-DOIT-DEV-KIT-v1-pinout-mischianti.png
-// TTGo-koala does not have built-in LED, just use a external LED instead
-// Pinout: https://ae01.alicdn.com/kf/HTB1OhKrbyYrK1Rjy0Fdq6ACvVXas.jpg 
+/* 
+ * Built-in LED on the ESP32 DEVKIT C1 is on pin 2
+ * pinout https://www.mischianti.org/wp-content/uploads/2020/11/ESP32-DOIT-DEV-KIT-v1-pinout-mischianti.png
+ * TTGo-koala does not have built-in LED, just use a external LED instead
+ * Pinout: https://ae01.alicdn.com/kf/HTB1OhKrbyYrK1Rjy0Fdq6ACvVXas.jpg 
+*/
 #define BLINK_GPIO 2
 
 void app_main() {
-    // Do your setup here
-    // Setup BLINK_GPIO as a output pin
+    /* Do your setup here */
+    /* Setup BLINK_GPIO as a output pin */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
-    // Main loop
+    /* Main loop */
     while (1) {
-        // set GPIO high
+        /* set GPIO high */
         gpio_set_level(BLINK_GPIO, 1);
-        // delay of 750 ms
+        /* delay of 750 ms */
         vTaskDelay(750 / portTICK_RATE_MS);
-        // set GPIO low
+        /* set GPIO low */
         gpio_set_level(BLINK_GPIO, 0);
-        // delay of 750 ms
+        /* delay of 750 ms */
         vTaskDelay(750 / portTICK_RATE_MS);
     }
 }
--- Examples_ESP/example_printf/src/main.c
+++ Examples_ESP/example_printf/src/main.c
@@ -1,20 +1,35 @@
-// Serial communication
-#include <stdio.h>
-// Include FreeRTOS for delay 
+/*
+ * Author:      Stefan de Kraker
+ * Version:     1.0
+ *
+ * Simple example of the use of printf
+ * It is important to add the following line to the `platformio.ini` file:
+ * 
+ * monitor_speed = 115200
+ * 
+ * This will set the baud rate to 115200 for the serial port.
+ * Also this is the default baud rate of the ESP32
+*/
+
+/* Needed to control GPIO */
+#include "driver/gpio.h"
+/* Include FreeRTOS for delay */
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+/* Serial communication is done by standard stdio */
+#include <stdio.h>
 
 void app_main(){
-    // Do your setup here
-    // Init of a simple uint8_t
+    /* Do your setup here */
+    /* Init of a simple uint8_t */
     uint8_t count = 0;
 
-    // Main loop    
+    /* Main loop */    
     while(1){
         printf("Hello world!\n"); 
         count = count + 1;
         printf("count now is: %i \r\n", count);
-        // added small delay of 0.5 seconds
+        /* added small delay of 0.5 seconds */
         vTaskDelay(500 / portTICK_RATE_MS);
     } 
 }
