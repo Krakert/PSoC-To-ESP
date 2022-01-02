a6c8209e067032e49d4a0fd7c72b0a11cc8352b0
Stefan de Kraker <stefandekraker1999@gmail.com>
2022-01-02 23:56:20 +0100

    Added better comments

--- Examples_ESP/Example_UART_receive/src/main.c
+++ Examples_ESP/Example_UART_receive/src/main.c
@@ -1,12 +1,21 @@
-/* UART Echo Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+/*
+ * Author:      Stefan de Kraker
+ * Version:     1.0
+ *
+ * Simple example of the use of UART
+ * It is important to add the following line to the `platformio.ini` file:
+ * 
+ * monitor_speed = 115200
+ * monitor_flags = 
+ *     --echo 
+ *     --filter
+ *     send_on_enter
+ *     --eol
+ *     CRLF
+ * 
+ * This will set the baud rate to 115200 for the serial port,
+ * and make is possible to send messages to the ESP32.
 */
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/uart.h"
@@ -23,26 +32,26 @@
 
 
 void app_main(void) {
-
+    /* Define the UART port, and the GPIO needed for it */
     uart_set_pin(UART_PORT_NUM, UART_PIN_TX, UART_PIN_RX, -1, -1);
+    /* Setup the driver, by defining buffers and if needed interrupts */
     uart_driver_install(UART_PORT_NUM, BUF_SIZE , BUF_SIZE, 0, NULL, 0);    
-
+    /* Write data to the UART port */ 
     uart_write_bytes(UART_PORT_NUM, "STARTED\n\r", strlen("STARTED\n\r"));
 
+    /* Configure a buffer for the incoming data */
+    char data[BUF_SIZE];
+    /*Configure a buffer for the string output */
     char line[BUF_SIZE];
 
-    /* Configure a temporary buffer for the incoming data */
-    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
-
     while (1) {
-        // Read data from the UART
+        /* Read data from the UART */
         int len = uart_read_bytes(UART_PORT_NUM, data, BUF_SIZE, 20 / portTICK_RATE_MS);
-        // Write data back to the UART
-        snprintf(line, len+10, "ECHO IS: %s\r\n\n", data);
-
-        if(len > 0)
+        /* Place data in a buffer */
+        snprintf(line, len + 10 ,"ECHO IS: %s\r\n\n", data);
+        /* If ther is data, write that back to the COM port */
+        if(len > 0) {
             uart_write_bytes(UART_PORT_NUM, (const char *) line, strlen(line));
-        
-        vTaskDelay(10 / portTICK_RATE_MS);
+        }
     } 
 }
